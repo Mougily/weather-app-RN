5eb55ec934c83e267c1d2c7048518c789ed9986d
Mougily <92stephaniejones@gmail.com>
2023-09-11 15:52:55 +0100

    installation of packages

--- App.js
+++ App.js
@@ -1,57 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from 'react'
+import { ActivityIndicator, View, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './src/components/Tabs'
+import { useGetWeather } from './src/hooks/useGetWeather'
+import ErrorItem from './src/components/ErrorItem'
 
-export default function App() {
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "pink",
-      flex: 1,
-      alignItems: "center",
-    },
-    wrapper: {
-      flex: 1,
-    },
+const App = () => {
+  const [loading, error, weather] = useGetWeather()
 
-    temp: {
-      color: "black",
-      fontSize: 48,
-    },
+  if (weather && weather.list && !loading) {
+    return (
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
+    )
+  }
 
-    feels: {
-      fontSize: 48,
-      color: "black",
-    },
-
-    highLow: {
-      color: "black",
-      fontSize: 20,
-    },
-    highLowWrapper: {
-      flexDirection: "row",
-    },
-    bodyWrapper: {
-      justifyContent : 'flex-end',
-      alignItems: 'flex-end',
-    },
-    description : {
-      fontSize: 48,
-    },
-    message : {
-     fontSize : 30,
-    }
-  });
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text style={styles.temp}>6</Text>
-        <Text stlye={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}> <Text style={styles.highLow}>High : 8    </Text>
-        <Text style={styles.highLow}>Low: 6</Text></View>
-       <View style={styles.bodyWrapper}>
-        <Text style={styles.description} >It's sunny</Text>
-        <Text style={styles.message}>It's perfect t-shirt weather</Text>
-       </View>
-      </View>
-    </SafeAreaView>
-  );
+    <View style={styles.container}>
+      {error ? (
+        <ErrorItem />
+      ) : (
+        <ActivityIndicator size={'large'} color={'blue'} />
+      )}
+    </View>
+  )
 }
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
+
+export default App
